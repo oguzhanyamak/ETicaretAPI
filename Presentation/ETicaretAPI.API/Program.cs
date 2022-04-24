6736060136d9321df66fc5c64f8e17c380a82af0
Oğuzhan Yamak <oguzhanyamak@outlook.com>
2022-04-24 18:06:40 +0300

    Cors Policy Tanımı ve Eklenmesi

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -3,6 +3,7 @@ using ETicaretAPI.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddPersistenceServices(); // IoC Container
 // Add services to the container.
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
