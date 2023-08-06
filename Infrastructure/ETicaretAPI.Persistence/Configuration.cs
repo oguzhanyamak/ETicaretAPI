0f9bd6da6ecdd4dceb0d7ac880019fb87b29e6dd
oguzhan yamak <oguzhanyamak@outlook.com>
2023-08-06 14:38:12 +0300

    Temel yapılanma ve Veritabanı

---
+++ Infrastructure/ETicaretAPI.Persistence/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    static class Configuration
+    {
+        static public string ConnectionString {
+            get 
+            {
+                ConfigurationManager manager = new();
+                manager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+                manager.AddJsonFile("appsettings.json");
+                return manager.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/DesingTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretAPI.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public class DesingTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretAPIDbContext>
+    {
+        public ETicaretAPIDbContext CreateDbContext(string[] args)
+        {
+
+            DbContextOptionsBuilder<ETicaretAPIDbContext> dbContextBuilder = new();
+            dbContextBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new(dbContextBuilder.Options);
+        }
+    }
+}
