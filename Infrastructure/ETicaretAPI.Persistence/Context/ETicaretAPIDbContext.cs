c272db13d647fb2ba322f405346cae6ba53d143d
oguzhan yamak <oguzhanyamak@outlook.com>
2023-08-07 18:34:10 +0300

    SaveChanges Interceptor

--- Infrastructure/ETicaretAPI.Persistence/Context/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Context/ETicaretAPIDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +17,37 @@ namespace ETicaretAPI.Persistence.Context
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
 
+        public override int SaveChanges()
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+            foreach (var item in datas)
+            {
+                if (item.State == EntityState.Added)
+                {
+                    item.Entity.CreatedDate = DateTime.UtcNow;
+                }
+                else if (item.State == EntityState.Modified)
+                {
+                    item.Entity.UpdatedDate = DateTime.UtcNow;
+                }
+            }
+            return base.SaveChanges();
+        }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+            foreach (var item in datas)
+            {
+                if (item.State == EntityState.Added)
+                {
+                    item.Entity.CreatedDate = DateTime.UtcNow;
+                }
+                else if (item.State == EntityState.Modified)
+                {
+                    item.Entity.UpdatedDate = DateTime.UtcNow;
+                }
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
